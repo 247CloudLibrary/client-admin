0d858ff3007634bd7a96e8406fc74fc54c0cb60c
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-04 15:22:53 +0900

    lib/api/boardInformation.js에 API 함수 작성

---
+++ src/lib/api/boardInformation.js
@@ -0,0 +1,41 @@
+import qs from "qs";
+import client from "./client";
+
+//게시글 등록
+export const writeInfoBoard = ({
+  type,
+  title,
+  contents,
+  libraryName,
+  adminName,
+}) => client.post("/boards", { type, title, contents, libraryName, adminName });
+
+//게시판 목록 조회
+export const listInfoBoards = ({
+  boardId,
+  title,
+  adminName,
+  createdAt,
+  updatedAt,
+  readCounts,
+}) => {
+  const queryString = qs.stringify({
+    boardId,
+    title,
+    adminName,
+    createdAt,
+    updatedAt,
+    readCounts,
+  });
+  return client.get(`/boards?${queryString}`);
+};
+
+//게시글 조회
+export const readInfoBoard = (boardId) => client.get(`/boards/${boardId}`);
+
+//게시글 수정
+export const updateInfoBoard = ({ boardId, type, title, contents }) =>
+  client.put(`/boards/${boardId}`, { type, title, contents });
+
+//게시글 삭제
+export const removeInfoBoard = (boardId) => client.delete(`/boards/${boardId}`);
