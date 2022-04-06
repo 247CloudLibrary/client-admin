122d63eec0ad5a893cf2702c218cc450975c6d40
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-06 16:47:51 +0900

    boardInformation.js 수정

--- src/lib/api/boardInformation.js
+++ src/lib/api/boardInformation.js
@@ -1,38 +1,66 @@
-import qs from "qs";
 import client from "./client";
 
 //게시글 등록
-export const writeInfoBoard = ({ type, title, contents, libraryName }) =>
-  client.post("/boards", { type, title, contents, libraryName });
-
-//게시판 목록 조회
-export const listInfoBoards = ({
+export const writeInfoBoard = ({
   boardId,
+  adminId,
+  adminName,
   type,
   title,
-  adminName,
+  contents,
+  libraryId,
+  libraryName,
   createdAt,
   updatedAt,
   readCounts,
-}) => {
-  const queryString = qs.stringify({
+}) =>
+  client.post("/boards", {
     boardId,
+    adminId,
+    adminName,
     type,
     title,
-    adminName,
+    contents,
+    libraryId,
+    libraryName,
     createdAt,
     updatedAt,
     readCounts,
   });
-  return client.get(`/boards?${queryString}`);
-};
+
+//게시판 목록 조회
+export const listInfoBoards = () => client.get("/boards");
 
 //게시글 조회
-export const readInfoBoard = (boardId) => client.get(`/boards/${boardId}`);
+export const readInfoBoard = () => client.get(`/boards/${boardId}`);
 
 //게시글 수정
-export const updateInfoBoard = ({ boardId, type, title, contents }) =>
-  client.put(`/boards/${boardId}`, { type, title, contents });
+export const updateInfoBoard = ({
+  boardId,
+  adminId,
+  adminName,
+  type,
+  title,
+  contents,
+  libraryId,
+  libraryName,
+  createdAt,
+  updatedAt,
+  readCounts,
+}) =>
+  client.put(`/boards/${boardId}`, {
+    boardId,
+    adminId,
+    adminName,
+    type,
+    title,
+    contents,
+    libraryId,
+    libraryName,
+    createdAt,
+    updatedAt,
+    readCounts,
+  });
 
 //게시글 삭제
 export const removeInfoBoard = (boardId) => client.delete(`/boards/${boardId}`);
