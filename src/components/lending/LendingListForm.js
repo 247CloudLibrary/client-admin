60335cc0cc10a9fdc08fffb17ed33bf7d6969e15
JoosangKim <fable0831@gmail.com>
2022-04-13 15:51:28 +0900

    form 작성
    1. LendingListForm 작성
    2. LendingListItem 작성
    3. LendingSearchBox 작성
    ------------------------------
    issue : 1. cors 에러 2. map을 인식 못함

--- src/components/lending/LendingListForm.js
+++ src/components/lending/LendingListForm.js
@@ -1,42 +1,37 @@
-
-const LendingItemForm = () => {
-    return (
-        <div>
-            <CheckBox>
-                
-            </CheckBox>
-            <Thumbnail>
-
-            </Thumbnail>
-            <Detail>
-
-            </Detail>
-            <Status-bar>
-
-            </Status-bar>
-        </div>
-    );
-};
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import LendingListItem from "./LendingListItem";
 
 const LendingListForm = () => {
-    return(
-        <div className="lendingListFormBlock"></div>
-            <ResultFilter> 이것은 필터입니다.</ResultFilter>
-            <div>
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-                <LendingItemForm />
-            </div>
-        </>
-    )
-    
-}
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://ecs-alb-167470959.us-east-1.elb.amazonaws.com/v1/lending")
+      .then((response) => setData(response.data));
+  });
+
+  return (
+    <div className="lending-list">
+      <div className="lendingListItem">검색</div>
+      {data.map(
+        ({data}) => (
+          <div className="list-box">
+            <LendingListItem
+              uid={data.uid}
+              thumnailImage={data.thumnailImage}
+              bookid={data.bookId}
+              libraryName={data.libraryName}
+              barcode={data.barcode}
+              lendingStatus={data.lendingStatus}
+              lendingDateTime={data.lendingDateTime}
+              returnDateTime={data.returnDateTime}
+            />
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
 export default LendingListForm;
---
+++ src/components/lending/LendingListItem.js
@@ -0,0 +1,32 @@
+
+const LendingListItem = ({
+  uid,
+  thumnailImage,
+  bookId,
+  libraryName,
+  barcode,
+  lendingStatus,
+  lendingDateTime,
+  returnDateTime,
+}) => {
+  return (
+    <div className="lening-items">
+      <input type="checkbox" value={bookId} />
+      <div className="thumnailImage">{thumnailImage}</div>
+      <div className="bookData">
+        <span>책 제목: {bookId}</span>
+        <span>도서관: {libraryName}</span>
+        <span>빌린이: {uid}</span>
+        <span>청구기호: {barcode}</span>
+        <span>대출일: {lendingDateTime}</span>
+        <span>반납예정일: {returnDateTime}</span>
+      </div>
+      <div className="bookStatusBar">
+        <span>상태: {lendingStatus}</span>
+        <span>연체 일수: </span>
+      </div>
+    </div>
+  );
+};
+
+export default LendingListItem;
