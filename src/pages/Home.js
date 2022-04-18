60da58166e8e706d26f3eba5d2052e012bb3aad0
JoosangKim <fable0831@gmail.com>
2022-04-18 17:54:18 +0900

    홈에 페이지 이동 버튼 추가
    1. 추후 CSS 작업(아이콘)
    2. searchbox 추가
    3. Header / Footer 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
-
 const Home = () => {
-    return (
-    <div>OOO 도서관</div>
-    )
+  return (
+    <div>
+      <h1>OOO 도서관</h1>
+      <Link to={"/boards"}>
+        <button>공지사항 관리</button>
+      </Link>
+      <Link to={"/books"}>
+        <button>도서 관리</button>
+      </Link>
+      <Link to={"/lending"}>
+        <button>대출 관리</button>
+      </Link>
+      <Link to={"/admin"}>
+        <button>마이 페이지</button>
+      </Link>
+      <Link to={"/lending/blacklist"}>
+        <button>블랙리스트</button>
+      </Link>
+      <Link to={"/lending/libraries/rules"}>
+        <button>이용 규정 관리</button>
+      </Link>
+      <Link to={"/auth"}>
+        <button>회원 조회</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
