780d5cb81b5893f773b0183685221d295d1a3ad6
iinsue <hinsu93@naver.com>
2022-04-19 11:33:30 +0900

    로그인 페이지 추가

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/style.scss";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "@redux-saga/core";
+import rootReducer, { rootSaga } from "./modules";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/lib/api/auth.js
@@ -0,0 +1,45 @@
+import client from "./client";
+
+export const login = ({ userId, password }) => {
+  client.post("/auth/signin", { userId, password });
+};
+
+export const profile = () => {
+  client.get("/auth");
+};
+
+export const updateProfile = ({
+  userName,
+  gender,
+  birth,
+  address,
+  email,
+  tel,
+}) => {
+  client.patch("/auth/update-state", {
+    userName,
+    gender,
+    birth,
+    address,
+    email,
+    tel,
+  });
+};
+
+// 회원 탈퇴
+export const deleteUser = () => {
+  client.delete("/auth/withdraw");
+};
+
+// 회원 아이디 찾기
+export const findid = ({ email }) => {
+  client.post("/auth/findId", { email });
+};
+
+// 회원 비밀번호 찾기
+export const findPassword = ({ userId, email }) => {
+  client.post("/auth/findPw", { userId, email });
+};
+
+// 로그인 상태 확인
+export const check = () => client.get("/auth/check");
