051a50aa690ccc1d0652d501f3df62f6f6f71627
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-22 16:25:25 +0900

    [board] axios 부분 연결

--- src/components/board/BoardWriteForm.js
+++ src/components/board/BoardWriteForm.js
@@ -2,8 +2,7 @@ import { Link } from "react-router-dom";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { useState } from "react";
-
-const LibraryName = "OOO 도서관";
+import axios from "axios";
 
 const BoardWriteForm = () => {
   const [boardContent, setBoardContent] = useState({
@@ -11,7 +10,7 @@ const BoardWriteForm = () => {
     title: "",
     contents: "",
   });
-
+  const libraryName = "임시 도서관";
   const { type, title, contents } = boardContent;
 
   const getValue = (e) => {
@@ -24,8 +23,26 @@ const BoardWriteForm = () => {
 
   return (
     <div className="BoardWrite">
-      <form>
-        <h1 className="library-name">{LibraryName}</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          axios
+            .post(
+              "http://ecs-alb-167470959.us-east-1.elb.amazonaws.com/v1/boards",
+              {
+                libraryName: libraryName,
+                type: type,
+                title: title,
+                contents: contents,
+              }
+            )
+            .then(function (response) {
+              console.log(response);
+              alert("게시글이 등록되었습니다.");
+            });
+        }}
+      >
+        <h1 className="library-name">{libraryName}</h1>
         <div className="type">
           <select
             value={type}
