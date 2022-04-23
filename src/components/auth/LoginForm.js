82072afa2d9a369fefd7cd2da0e711e884fc09bd
Han-su512 <js7122420@gmail.com>
2022-04-24 05:44:37 +0900

    login form path 변경

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,33 +1,21 @@
-import { Link } from "react-router-dom";
-import { WiCloud } from "react-icons/wi";
-import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { WiCloud } from 'react-icons/wi';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = ({ onChange, onSubmit, form }) => {
   const navigate = useNavigate();
   const onClick = () => {
-    navigate("/home");
+    navigate('/home');
   };
   return (
     <div className="loginForm">
-      <Link to={"/"} className="logo">
+      <Link to={'/clientAdmin-service'} className="logo">
         <WiCloud />
         <span>Cloud Library</span>
       </Link>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="아이디"
-          name="userId"
-          onChange={onChange}
-          value={form.userId}
-        />
-        <input
-          onChange={onChange}
-          value={form.password}
-          name="password"
-          type="password"
-          placeholder="비밀번호"
-        />
+        <input type="text" placeholder="아이디" name="userId" onChange={onChange} value={form.userId} />
+        <input onChange={onChange} value={form.password} name="password" type="password" placeholder="비밀번호" />
         <button className="login-btn" onClick={onClick}>
           로그인
         </button>
