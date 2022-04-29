059f0f0f45b603a24eddba9e0bf81ea5e4da9b10
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-04-29 14:51:57 +0900

    [libraries] 검색기능 수정 및 백그라운드 수정

---
+++ src/components/board/Board.js
@@ -0,0 +1,39 @@
+import { Navigate, useNavigate } from "react-router-dom";
+import { BsMegaphone, BsInfoCircle } from "react-icons/bs";
+import { IoLibraryOutline } from "react-icons/io5";
+
+const Board = () => {
+  const navigate = useNavigate();
+
+  const toLibNotice = () => {
+    navigate("/boards/list");
+  };
+  const toNotice = () => {
+    navigate("/notices");
+  };
+  const toGuide = () => {
+    navigate("/guide");
+  };
+
+  return (
+    <div id="Board">
+      <div className="btn-box">
+        <div className="btn">
+          <div onClick={toLibNotice}>
+            <IoLibraryOutline className="icon" />
+            <span className="tag">도서관 공지사항</span>
+          </div>
+          <div onClick={toNotice}>
+            <BsMegaphone className="icon" />
+            <span className="tag">통합 공지사항</span>
+          </div>
+          <div onClick={toGuide}>
+            <BsInfoCircle className="icon" />
+            <span className="tag">통합 이용안내</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Board;
--- src/pages/board/BoardsPage.js
+++ src/pages/board/BoardsPage.js
@@ -1,20 +1,12 @@
-import { Link } from "react-router-dom";
+import Board from "../../components/board/Board";
 import Footer from "../../components/Footer";
 
 const BoardsPage = () => {
   return (
-    <div>
-      <Link to={"/boards/list"}>
-        <button>개별 도서관 공지사항 관리</button>
-      </Link>
-      <Link to={"/notices"}>
-        <button>통합 공지사항</button>
-      </Link>
-      <Link to={"/guide"}>
-        <button>통합 이용안내</button>
-      </Link>
+    <>
+      <Board />
       <Footer />
-    </div>
+    </>
   );
 };
 
