a81ab82108602cf7f5077f996b12e8a2462c0197
iinsue <hinsu93@naver.com>
2022-05-04 10:17:52 +0900

    비밀번호 변경 추가 & 도서 검색 추가

---
+++ src/components/common/SearchBar.js
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const onSearch = (event) => {
+    event.preventDefault();
+    navigate("/composite-list");
+  };
+
+  return (
+    <form className="home__search-bar">
+      <input name="search-home" type="text" autoComplete="off" required />
+      <label htmlFor="search-home" className="label-name">
+        <span className="content-name">Book Title</span>
+      </label>
+      <BiSearch className="icon" />
+    </form>
+  );
+};
+
+export default SearchBar;
